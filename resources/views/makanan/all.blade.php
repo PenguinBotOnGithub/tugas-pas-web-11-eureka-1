db3f80481f44a10116e874b9578214424b786541
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-11-14 13:31:03 +0700

    Perbaiki tabel all makanan karena lupa <tr>

--- resources/views/makanan/all.blade.php
+++ resources/views/makanan/all.blade.php
@@ -10,11 +10,13 @@
             <th>Action</th>
         </tr>
         @foreach ($makanans as $index => $item)
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $item->nama }}</td>
-            <td><img src="{{ $item->gambar }}" alt="{{ $item->nama . '-alt' }}" class="img-thumbnail w-25"></td>
-            <td>{{ $item->asal_daerah }}</td>
-            <td><a type="button" href="/makanan/detail/{{ $item->id }}" class="btn btn-primary">Detail</a></td>
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $item->nama }}</td>
+                <td><img src="{{ $item->gambar }}" alt="{{ $item->nama . '-alt' }}" class="img-thumbnail w-25"></td>
+                <td>{{ $item->asal_daerah }}</td>
+                <td><a type="button" href="/makanan/detail/{{ $item->id }}" class="btn btn-primary">Detail</a></td>
+            </tr>
         @endforeach
     </table>
 @endsection
